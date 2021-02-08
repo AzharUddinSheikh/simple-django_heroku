739b72bb4eb41f23ad9ff7f1ce6225e84e25b9e2
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-09 01:04:07 +0530

    created model not yet migrate

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    year = models.IntegerField()
+    rating = models.FloatField()
+    stock_available = models.IntegerField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
