c340b11fcb978f5a7c29062195a96cf762e7b3e1
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-09 16:12:14 +0530

    we set path to setting for our api app, in we provided path with the help of class meta some attribute we mention what to include and exclude

--- moviesite/urls.py
+++ moviesite/urls.py
@@ -23,5 +23,5 @@ movie_resource = MovieResource()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("movies/", include("movies.urls")),
-    path("api/", include("movie_resource.urls"))
+    path("api/", include(movie_resource.urls))
 ]
